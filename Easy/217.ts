baebdc3f59b1c490dc3a2363c60fc15a28d29c61
jamilhossain1997 <jamilhossain4792@gmail.com>
2026-09-01 22:09:56 +0600

    LeetCode Array 217 solve typeScript

---
+++ Easy/217.ts
@@ -0,0 +1,17 @@
+function containsDuplicate(nums: number[]): boolean {
+   const seen = new Set<number>();
+
+   for(const num of nums){
+
+      if(seen.has(num)){
+        return true;
+      }
+
+      seen.add(num);
+   }
+
+ return false;
+};
+ 
+console.log(containsDuplicate([1, 2, 3, 1])); // true
+console.log(containsDuplicate([1, 2, 3, 4]));
