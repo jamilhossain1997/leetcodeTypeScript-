f7c7c78aed1658ab6c9331fb4f848c3f5ed480dc
jamilhossain1997 <jamilhossain4792@gmail.com>
2026-09-02 22:32:14 +0600

    submit leetCode 238 solved

---
+++ test.ts
@@ -0,0 +1,18 @@
+function twoSum1 (nums: number[], target: number): number[]{
+    const seen = new Map<number,number>(); // value -> index 
+
+    for (let i=0; i<nums.length; i++){
+        const complement = target - nums[i];
+         
+        if(seen.has(complement)){
+            return [seen.get(complement)!,i];
+        }
+        seen.set(nums[i],i);
+    }
+    
+    return [];
+}
+
+console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
+console.log(twoSum([3, 2, 4], 6));      // [1, 2]
+console.log(twoSum([3, 3], 6));  
