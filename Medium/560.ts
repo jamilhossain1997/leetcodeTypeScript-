cde5e5e7c648317817906376570dd0e7298e4fd8
jamilhossain1997 <jamilhossain4792@gmail.com>
2026-09-10 00:03:01 +0600

    560 resolved

--- Medium/560.ts
+++ Medium/560.ts
@@ -1,21 +1,18 @@
-function subarraySumEqual(nums: number [], k: number): number {
-    const seen = new Map < number, number > ();
+function subarraySumEqual(nums: number [],k: number):number{
+    const seen =new Map<number,number>();
     seen.set(0,1);
-
     let currentSum = 0;
     let count = 0;
+
     for(const num of nums){
-        currentSum += num;
+        currentSum +=num;
 
         if(seen.has(currentSum-k)){
-            count += seen.get(currentSum-k) !;
+            count +=seen.get(currentSum-k)!;
         }
 
         seen.set(currentSum,(seen.get(currentSum) ?? 0)+1);
-
-
     }
-
     return count;
 }
 
