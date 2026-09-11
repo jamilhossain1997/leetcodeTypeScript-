a8faf55980e571a54a551937742ac2d5c098dbc6
jamilhossain1997 <jamilhossain4792@gmail.com>
2026-09-12 02:58:06 +0600

    resolved Easy 217

--- Easy/217.ts
+++ Easy/217.ts
@@ -1,9 +1,9 @@
 function containsDuplicate(nums: number[]): boolean {
-   const seen = new Set<number >();
+   const seen = new Set<number> ();
 
-   for(const num of nums){
+   for(const num of nums ){
      if(seen.has(num)){
-       return true;
+      return true;
      }
 
      seen.add(num);
