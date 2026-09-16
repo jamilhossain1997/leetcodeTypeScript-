37ec078b630714454ee5b8eb4f633e6caa48fcce
jamilhossain1997 <jamilhossain4792@gmail.com>
2026-09-17 00:44:26 +0600

    15 no leetCode solved

--- Medium/15.ts
+++ Medium/15.ts
@@ -1,34 +1,36 @@
 function threeSum(nums:number[]): number [][]{
-    nums.sort((a,b)=>a-b);
-    const n =nums.length;
-    const result: number [][] =[];
+  nums.sort((a,b)=>a-b);
+  const result:number[][]=[];
+  const n = nums.length;
 
-    for(let i = 0; i< n-2; i++){
-        if(nums[i]>0) break;
+  for(let i = 0; i< n-2; i++){
+     if(nums[i]>0) break;
+
+     if(i > 0 && nums[i]==nums[i-1]) continue ;
+     let left = i+1;
+     let right = n-1;
+     while(left < right){
+        const total = nums[i]+nums[left]+nums[right];
         
-        if(i > 0 && nums[i]==nums[i-1]) continue;
+        if(total == 0){
+            result.push([nums[i],nums[left],nums[right]]);
+            
+            left++ ;
+            right-- ;
 
-        let left = i+1;
-        let right = n-1;
+            while(left < right && nums[left] == nums[left -1]) left++ ;
+            while(left < right && nums[right] == nums[right+1]) right-- ;
 
-        while(left < right){
-            const total = nums[i]+nums[left]+nums[right] ;
-
-            if(total === 0){
-                result.push([nums[i],nums[left],nums[right]]);
-                left++ ;
-                right-- ;
-
-                while(left < right && nums[left]=== nums[left -1]) left++ ;
-                while(left < right && nums[right]==nums[right-1]) right-- ;
-
-            }else if(total < 0){
-                left++ ;
-            }else{
-                right-- ;
-            }
+        }else if( total < 0){
+            left++ ;
+        }else{
+            right-- ;
         }
-    }
+
+     }
+  }
+
+
 
     return result;
 }
